3135754d2aeba6bea4812bc2e3ec38d341485ee1
DNS <37093+DNS@users.noreply.github.com>
2025-12-23 13:49:25 +0700

    A: rdap.ps1

---
+++ rdap.ps1
@@ -0,0 +1,40 @@
+
+if ($args) {
+	$bulk_domain = $args
+} else {
+	$bulk_domain = $input | % { $_ }
+}
+
+
+if (-not $bulk_domain) {
+	Write-Output 'rdap.ps1 — Check domain expiration date using `rdap.net`'
+	Write-Output 'Usage:'
+	Write-Output '	rdap abc.com xyz.com'
+	Write-Output "	'abc.com', 'xyz.com' | rdap"
+	exit
+}
+
+
+foreach ($domain in $bulk_domain) {
+	$a = irm "https://www.rdap.net/domain/$domain"
+	
+	$a.events | % {
+		if ($_.eventAction -match 'regist') {
+			$registration = $_.eventDate.ToString('yyyy-MM-dd')
+		}
+		
+		if ($_.eventAction -match 'expir') {
+			$expiration = $_.eventDate.ToString('yyyy-MM-dd')
+		}
+		
+	}
+	
+	[PSCustomObject] @{
+		domain = $domain
+		registration = $registration
+		expiration = $expiration
+	}
+
+}
+
+
